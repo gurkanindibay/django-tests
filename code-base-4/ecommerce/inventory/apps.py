15df4a1f41bc1622d935127975580cb6ebdcb8cd
Gurkan Indibay <gindibay@microsoft.com>
2023-01-19 04:32:50 +0300

    Removed pycache files

--- code-base-4/ecommerce/inventory/apps.py
+++ code-base-4/ecommerce/inventory/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class QuickstartConfig(AppConfig):
+class InventoryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "tutorial.quickstart"
+    name = "ecommerce.inventory"
